1bc2d467e439ad40791039935ac7e1b5446ba077
Ahmed A <a.abdi11965@gmail.com>
2023-11-14 17:17:39 -0500

    corrections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from "hooks/useApplicationData";
+import axios from 'axios';
 
 import './App.scss';
 
 const App = () => {
+  // useEffect(() => {
+  //   axios.get(`/api/topics`).then(res => {
+  //     console.log(123);
+  //     console.log('res value is', res);
+
+  //   }).catch(err => console.log('Unexpected error code', err))
+  // }, [])
+
+  useEffect(() => {
+    const topicsApi = `/api/topics`;
+    const photosApi = `/api/photos`;
+
+    Promise.all([axios.get(topicsApi), axios.get(photosApi)]).then((all) => {
+      const [topics, photos] = all;
+      console.log(all);
+      // setAppData(topics.data, photos.data);
+    }).catch(err => console.log("An unexpected error occured", err))
+  }, []);
+
+
+
 
   const {
     mainPagePhotos,
